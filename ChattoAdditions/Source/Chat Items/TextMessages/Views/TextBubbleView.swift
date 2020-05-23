4dd2d5653a34733aea65b5146a14aeacbee05461
Eric Silverberg <eric@scruff.com>
2020-05-23 19:33:34 -0400

    Expose textView so that we can hook into his delegate and intercept shouldInteractWithURL callbacks

--- ChattoAdditions/Source/Chat Items/TextMessages/Views/TextBubbleView.swift
+++ ChattoAdditions/Source/Chat Items/TextMessages/Views/TextBubbleView.swift
@@ -93,7 +93,7 @@ public final class TextBubbleView: UIView, MaximumLayoutWidthSpecificable, Backg
     }()
 
     private var borderImageView: UIImageView = UIImageView()
-    private var textView: UITextView = {
+    public var textView: UITextView = {
         let textView = ChatMessageTextView()
         UIView.performWithoutAnimation({ () -> Void in // fixes iOS 8 blinking when cell appears
             textView.backgroundColor = UIColor.clear
