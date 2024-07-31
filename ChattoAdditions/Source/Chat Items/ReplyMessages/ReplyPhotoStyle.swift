afe10258a65e37351e60ac0dc3cf9d44a8bcddad
Lucas Cavalcante <co.lucasc@gmail.com>
2024-07-31 12:38:56 -0300

    Fix ReplyPhotoStyle to calculate bubble size based on original bubble size.

--- ChattoAdditions/Source/Chat Items/ReplyMessages/ReplyPhotoStyle.swift
+++ ChattoAdditions/Source/Chat Items/ReplyMessages/ReplyPhotoStyle.swift
@@ -2,10 +2,16 @@ import Foundation
 import Chatto
 
 public final class ReplyPhotoStyle: PhotoMessageCollectionViewCellDefaultStyle {
+    private let resizePercentage: Double
+    
+    public init(resizePercentage: Double = 0.5) {
+        self.resizePercentage = resizePercentage
+    }
+    
     override public func bubbleSize(viewModel: any PhotoMessageViewModelProtocol) -> CGSize {
-        let width = viewModel.imageSize.width * 0.5
-        let height = viewModel.imageSize.height * 0.5
-
+        let defaultSize = super.bubbleSize(viewModel: viewModel)
+        let width = defaultSize.width * resizePercentage
+        let height = defaultSize.height * resizePercentage
         return CGSize(width: width, height: height)
     }
 }
